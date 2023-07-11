29a344776064368c87bab743f798eb08e8b5fdd0
edoardope <pencoedoardo@gmail.com>
2023-07-11 17:26:35 +0200

    log in + log out

--- src/App.vue
+++ src/App.vue
@@ -11,28 +11,52 @@ export default {
   data() {
     return {
       store,
+      name: "leonardocx",
+      email: "santo@leoleo",
+      password: "leonardo",
+      token: localStorage['token'],
     }
   },
   created() {
-    this.createLeo()
+    
   },
   methods: {
     getImagePath: function (imgPath) {
       return new URL(imgPath, import.meta.url).href;
     },
-    createLeo() {
-      axios.post('http://127.0.0.1:8000/api/auth/login', {
-        name: 'leonardocx1',
-        email: 'santo1@leoleo',
-        password: 'leonardo'
-      }).then(res=>(console.log(res)))
-    }
+    login() {
+      if (!localStorage['token']) {
+
+        axios.post('http://127.0.0.1:8000/api/auth/login', {
+          name: this.name,
+          email: this.email,
+          password: this.password
+        }).then(res=>(
+          console.log(res.data.message),
+          localStorage['token'] = res.data.token,
+          this.token = localStorage['token']
+        ))
+      } else {
+        console.log("gia loggato")
+      }
+
+      },
+      logout(){
+        localStorage['token'] = '',
+        this.token = ''
+      }
   }
 }
 </script>
 
 <template>
-  
+    <input  v-model="this.name" type="text" name="" id="">
+    <input v-model="this.email" type="text" name="" id="">
+    <input v-model="this.password" type="text" name="" id="">
+    <input @click="login()" type="button" value="login">
+    <input @click="logout()" type="button" value="logout">
+    <p v-if="(this.token)">your token is: {{this.token}}</p>
+
 </template>
 
 <style lang="scss">
